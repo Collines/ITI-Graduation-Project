f5cf394962cd0ae63073258387179a3cb420246e
Khaled Ahmed Mohamed <khaled1541998@gmail.com>
2023-05-18 11:30:20 +0300

    Added Modules To app.module & Created Doctor Service & Created Gender Enum

---
+++ Client/src/app/Services/doctors.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorsService {
+
+  constructor(private Clint:HttpClient) { }
+
+  private BaseURL = "https://localhost:7035/api/Doctors"
+
+  GetAllDoctors(){
+    return this.Clint.get(this.BaseURL);
+  }
+
+  GetById(id:number){
+    return this.Clint.get(`${this.BaseURL}/${id}`);
+  }
+
+  AddDoctor(NewDoctor:any){
+    return this.Clint.post(this.BaseURL,NewDoctor);
+  }
+
+  DeleteDoctor(id:number){
+    return this.Clint.delete(`${this.BaseURL}/${id}`);
+  }
+
+  EditDoctor(id:number,NewDoctor:any){
+    return this.Clint.patch(`${this.BaseURL}/${id}`,NewDoctor);
+  }
+}
