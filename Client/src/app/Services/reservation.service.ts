07646a0e263d8411e04e46ffae97386135eb4f98
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-05-28 22:12:12 +0300

    Update reservation.service.ts

--- Client/src/app/Services/reservation.service.ts
+++ Client/src/app/Services/reservation.service.ts
@@ -10,19 +10,19 @@ import { Headers } from '../utils/headers.utils';
 export class ReservationService {
   constructor(
     private http: HttpClient,
-    private header: HeaderService,
+    // private header: HeaderService,
     private headers: Headers
   ) {}
 
   private BaseURL = 'https://localhost:7035/api/Reservation';
-  private Header: HttpHeaders = this.header.Header;
+  // private Header: HttpHeaders = this.header.Header;
   private Headers = this.headers.getHeaders();
 
   GetAllReservations(id: number, accessToken: string) {
     return this.http.post<Reservation[]>(
       this.BaseURL + '/My-Reservations?patientId=' + id,
       `\"${accessToken}\"`,
-      { headers: this.Header.set('Authorization', `bearer ${accessToken}`) }
+      { headers: this.Headers.set('Authorization', `bearer ${accessToken}`) }
     );
   }
 
@@ -30,7 +30,7 @@ export class ReservationService {
     return this.http.patch<Reservation>(
       this.BaseURL + '/CancelReservation/' + id,
       reservation,
-      { headers: this.Header.set('Authorization', `bearer ${accessToken}`) }
+      { headers: this.Headers.set('Authorization', `bearer ${accessToken}`) }
     );
   }
 
@@ -38,7 +38,7 @@ export class ReservationService {
     return this.http.post(
       this.BaseURL + `?accessToken=${accessToken}`,
       reservation,
-      { headers: this.Header.set('Authorization', `bearer ${accessToken}`) }
+      { headers: this.Headers.set('Authorization', `bearer ${accessToken}`) }
     );
   }
 
