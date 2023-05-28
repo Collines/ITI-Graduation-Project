fc69511b7ead0631c602a04255637bc7c1008fdb
Ahmed Gamil <ah.k.gamil@gmail.com>
2023-05-28 13:26:47 +0300

    Updated Departments SQL queries.

--- Dataset/DepartmentTranslations.sql
+++ Dataset/DepartmentTranslations.sql
@@ -1,6 +1,12 @@
-﻿INSERT INTO DepartmentTranslations(Id, DepartmentId, Title_EN, Title_AR, Description_EN, Description_AR)
-VALUES (1, 1, N'Cardiology', N'قلبية', N'Department specializing in heart-related conditions.', N'قسم متخصص في الحالات المتعلقة بالقلب'),
-(2, 2, N'Orthopedics', N'عظام', N'Department specializing in bone and joint disorders.', N'قسم متخصص في اضطرابات العظام والمفاصل'),
-(3, 3, N'Gynecology', N'نسائية', N'Department specializing in women health and reproductive system.', N'قسم متخصص في صحة المرأة والجهاز التناسلي'),
-(4, 4, N'Pediatrics', N'أطفال', N'Department specializing in medical care for infants, children, and adolescents.', N'قسم متخصص في الرعاية الطبية للرضع والأطفال والمراهقين'),
-(5, 5, N'Neurology', N'عصبية', N'Department specializing in disorders of the nervous system.', N'قسم متخصص في اضطرابات الجهاز العصبي');
+﻿INSERT INTO DepartmentTranslations (Id, Title_EN, Title_AR, Description_EN, Description_AR, DepartmentId)
+VALUES
+    (1, N'Cardiology', N'طب القلب', N'Department specialized in heart diseases', N'قسم متخصص في أمراض القلب', 1),
+    (2, N'Orthopedics', N'طب العظام', N'Department specialized in bone and joint disorders', N'قسم متخصص في أمراض العظام والمفاصل', 2),
+    (3, N'Ophthalmology', N'طب العيون', N'Department specialized in eye diseases', N'قسم متخصص في أمراض العيون', 3),
+    (4, N'Gastroenterology', N'طب الجهاز الهضمي', N'Department specialized in digestive system disorders', N'قسم متخصص في أمراض الجهاز الهضمي', 4),
+    (5, N'Neurology', N'طب الأعصاب', N'Department specialized in nervous system disorders', N'قسم متخصص في أمراض الجهاز العصبي', 5),
+    (6, N'Oncology', N'طب الأورام', N'Department specialized in cancer treatment', N'قسم متخصص في علاج السرطان', 6),
+    (7, N'Dermatology', N'طب الأمراض الجلدية', N'Department specialized in skin disorders', N'قسم متخصص في أمراض الجلد', 7),
+    (8, N'Obstetrics and Gynecology', N'طب التوليد وأمراض النساء', N'Department specialized in women health', N'قسم متخصص في صحة المرأة', 8),
+    (9, N'Pediatrics', N'طب الأطفال', N'Department specialized in child healthcare', N'قسم متخصص في رعاية الأطفال', 9),
+    (10, N'Psychiatry', N'طب الطب النفسي', N'Department specialized in mental health', N'قسم متخصص في الصحة العقلية', 10);
--- Dataset/Departments.sql
+++ Dataset/Departments.sql
@@ -1,6 +1,12 @@
-INSERT INTO Departments(Title, Description, NoOfBeds)
-VALUES (N'Cardiology', N'Department specializing in heart-related conditions', 5),
-(N'Orthopedics', N'Department specializing in bone and joint disorders', 10),
-(N'Gynecology', N'Department specializing in women health and reproductive system', 15),
-(N'Pediatrics', N'Department specializing in medical care for infants, children, and adolescents', 20),
-(N'Neurology', N'Department specializing in disorders of the nervous system', 25);
+INSERT INTO Departments (Title, Description, NoOfBeds)
+VALUES
+    (N'Cardiology', N'Department specialized in heart diseases', 10),
+    (N'Orthopedics', N'Department specialized in bone and joint disorders', 12),
+    (N'Ophthalmology', N'Department specialized in eye diseases', 8),
+    (N'Gastroenterology', N'Department specialized in digestive system disorders', 9),
+    (N'Neurology', N'Department specialized in nervous system disorders', 7),
+    (N'Oncology', N'Department specialized in cancer treatment', 11),
+    (N'Dermatology', N'Department specialized in skin disorders', 6),
+    (N'Obstetrics and Gynecology', N'Department specialized in women health', 15),
+    (N'Pediatrics', N'Department specialized in child healthcare', 13),
+    (N'Psychiatry', N'Department specialized in mental health', 10);
