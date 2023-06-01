17a2eea7cf36fc124534b11fca54e9c21464e3ff
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-06-02 02:00:51 +0300

    Implemented Message Controller

---
+++ GraduationProject/Controllers/MessageController.cs
@@ -0,0 +1,55 @@
+﻿using GraduationProject_BL.DTO.BannerDTOs;
+using GraduationProject_BL.Interfaces;
+using GraduationProject_DAL.Data.Enums;
+using GraduationProject_DAL.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles ="Admin")]
+    public class MessageController : ControllerBase
+    {
+        public MessageController(IMessageManager manager)
+        {
+            Manager = manager;
+        }
+
+        public IMessageManager Manager { get; }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Message>>> GetAll()
+        {
+            var messages = await Manager.GetAllAsync();
+            if(messages!=null && messages.Count > 0)
+                return Ok(messages);
+            return NotFound();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<List<Message>>> GetById(int id)
+        {
+            var message = await Manager.GetByIdAsync(id);
+            if (message != null)
+                return Ok(message);
+            return NotFound();
+        }
+
+        [HttpPatch("ChangeStatus/{id:int}")]
+        public async Task<ActionResult> ChangeStatus(int id,Message msg)
+        {
+            var message = await Manager.GetByIdAsync(id);
+            if (message != null && msg.Id == id)
+            {
+                message.Status = msg.Status;
+                await Manager.UpdateAsync(id,message);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+    }
+}
