6993dff087a0caa75e5a0b841455783e9246302c
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-06-02 01:50:17 +0300

    Added Message Manager

---
+++ GraduationProject_BL/Managers/MessageManager.cs
@@ -0,0 +1,37 @@
+﻿using GraduationProject_BL.Interfaces;
+using GraduationProject_DAL.Data.Models;
+using GraduationProject_DAL.Interfaces;
+
+namespace GraduationProject_BL.Managers
+{
+    public class MessageManager : IMessageManager
+    {
+        public MessageManager(IRepository<Message> repository)
+        {
+            Repository = repository;
+        }
+
+        public IRepository<Message> Repository { get; }
+
+        public async Task DeleteAsync(int id)
+        {
+            await Repository.DeleteAsync(id);
+        }
+
+        public async Task<List<Message>> GetAllAsync()
+        {
+            return await Repository.GetAllAsync();
+        }
+
+        public async Task<Message?> GetByIdAsync(int id)
+        {
+            var messages = await Repository.GetAllAsync();
+            return messages.Find(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(int id, Message item)
+        {
+            await Repository.UpdateAsync(id, item);
+        }
+    }
+}
