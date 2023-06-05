d75ab413b55cd4ba911164858baa73432ad6f63c
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-06-06 00:23:49 +0300

    Added Add/Edit/View  Article operations

--- Dashboard/src/app/views/news-views/edit-news/edit-news.component.ts
+++ Dashboard/src/app/views/news-views/edit-news/edit-news.component.ts
@@ -1,85 +1,118 @@
-import { Component,OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ArticleService } from "src/app/services/article.service";
+import { ArticleEdit } from "src/app/interfaces/ArticleEdit";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-edit-news',
-  templateUrl: './edit-news.component.html',
-  styleUrls: ['./edit-news.component.scss']
+  selector: "app-edit-news",
+  templateUrl: "./edit-news.component.html",
+  styleUrls: ["./edit-news.component.scss"],
 })
-
-export class EditNewsComponent implements OnInit{
-  BaseImage:any = "";
-  FileToUpload:any;
+export class EditNewsComponent implements OnInit {
+  BaseImage: any = "";
+  FileToUpload: any;
   submitted = false;
-  constructor() {}
+  constructor(
+    private Route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
   ngOnInit(): void {
-   
+    this.articleService
+      .getEditData(this.Route.snapshot.params["id"])
+      .subscribe({
+        next: (res) => {
+          this.new = res;
+        },
+        error: (e) => console.log(e),
+      });
+    this.Validation.controls["Title"].value = this.new?.title_EN;
+    this.Validation.controls["TitleAR"].value = this.new?.title_AR;
+    this.Validation.controls["Body"].value = this.new?.description_EN;
+    this.Validation.controls["BodyAR"].value = this.new?.description_AR;
+
+    this.Validation.controls["Title"].status = "VALID";
+    this.Validation.controls["TitleAR"].status = "VALID";
+    this.Validation.controls["Body"].status = "VALID";
+    this.Validation.controls["BodyAR"].status = "VALID";
+    this.Validation.controls["Image"].status = "VALID";
+
+    this.Validation.status = "VALID";
   }
+  new: ArticleEdit | null = null;
+  private ArabicPattern = /^[[ء-ي\s]+$/;
+  private ArabicPatternForParagraph = /^[[ء-ي]|\s]|\.|\,+$/;
 
-    // Pattern For Arabic Letters Only
-    private ArabicPattern = /^[[ء-ي\s]+$/;
-    private ArabicPatternForParagraph = /^[[ء-ي]|\s]|\.|\,+$/;
-
-  isLogged = false;
-  Validation = new FormGroup({
+  Validation: any = new FormGroup({
     Image: new FormControl(null, [Validators.required]),
-    Title: new FormControl(null,
-      [Validators.required,
-        Validators.minLength(10)]),
-    TitleAR: new FormControl(null,
-      [Validators.required,
-        Validators.minLength(10),
-        Validators.pattern(this.ArabicPatternForParagraph)]),
-    Body: new FormControl(null,
-      [Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(500)]),
-    BodyAr: new FormControl(null,
-      [Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(500),
-        Validators.pattern(this.ArabicPatternForParagraph)]),
-    Date: new FormControl(null, [Validators.required]),
+    Title: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    TitleAR: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.pattern(this.ArabicPatternForParagraph),
+    ]),
+    Body: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(500),
+    ]),
+    BodyAr: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(500),
+      Validators.pattern(this.ArabicPatternForParagraph),
+    ]),
   });
-  get isImageValid(){
-    return this.Validation.controls.Image.valid;
+  get isImageValid() {
+    return this.Validation.controls["Image"].status == "VALID";
   }
   get isTitleValid() {
-    // return this.Validation.controls.Title.valid;
-    return this.Validation.controls.Title.valid
+    return this.Validation.controls["Title"].status == "VALID";
   }
   get isTitleArValid() {
-    return this.Validation.controls.TitleAR.valid;
+    return this.Validation.controls["TitleAR"].status == "VALID";
   }
   get isBodyValid() {
-    return this.Validation.controls.Body.valid;
+    return this.Validation.controls["Body"].status == "VALID";
   }
   get isBodyArValid() {
-    return this.Validation.controls.BodyAr.valid;
-  }
-  get isDateValid() {
-    return this.Validation.controls.Date.valid;
+    return this.Validation.controls["BodyAr"].status == "VALID";
   }
 
-  async ChangeSrc(e:any) {
+  async ChangeSrc(e: any) {
     var file = e.target.files[0];
-    if (file)
-      {
-        this.BaseImage = await this.ConvertBase64(file);
-        this.FileToUpload = file;
-      }
+    if (file) {
+      this.BaseImage = await this.ConvertBase64(file);
+      this.FileToUpload = file;
+    }
   }
-  ConvertBase64 = (file:any) => {
+  ConvertBase64 = (file: any) => {
     return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
+      const fileReader = new FileReader();
 
-    fileReader.readAsDataURL(file);
-    fileReader.onload = () => resolve(fileReader.result);
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => resolve(fileReader.result);
 
-    fileReader.onerror = (error) => reject(error);
+      fileReader.onerror = (error) => reject(error);
     });
-  }
+  };
   onSubmit() {
-   this.submitted=true
-}
+    this.submitted = true;
+    if (this.Validation.valid && this.new) {
+      const formData: FormData = new FormData();
+      if (this.FileToUpload) formData.append("Image", this.FileToUpload);
+      else return;
+      formData.append("Title_EN", this.Validation.controls.Title.value!);
+      formData.append("Title_AR", this.Validation.controls.TitleAR.value!);
+      formData.append("Description_EN", this.Validation.controls.Body.value!);
+      formData.append("Description_AR", this.Validation.controls.BodyAr.value!);
+      this.articleService.update(this.new!.id, formData).subscribe({
+        next: (res) => {},
+        error: (e) => console.log(e),
+      });
+    }
+  }
 }
--- Dashboard/src/app/views/news-views/news/news.component.ts
+++ Dashboard/src/app/views/news-views/news/news.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Article } from "src/app/interfaces/Article";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.scss"],
 })
 export class NewsComponent {
-
+  @Input() new: Article = {
+    id: 0,
+    title: "",
+    description: "",
+    image: "",
+    postedAt: "",
+  };
 }
