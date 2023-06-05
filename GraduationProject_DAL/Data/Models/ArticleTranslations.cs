7284efb3a5604f2922a1902a3e456124e08b61c5
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-06-05 19:15:17 +0300

    Created Article Entities and Repositories

---
+++ GraduationProject_DAL/Data/Models/ArticleTranslations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraduationProject_DAL.Data.Models
+{
+    [PrimaryKey(nameof(Id), nameof(ArticleId))]
+    public class ArticleTranslations
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public required string Title_EN { get; set; }
+
+        [Required, MaxLength(255)]
+        public required string Title_AR { get; set; }
+
+        [Required]
+        public required string Description_EN { get; set; }
+
+        [Required]
+        public required string Description_AR { get; set; }
+
+        [ForeignKey("Article")]
+        public required int ArticleId { get; set; }
+
+        public virtual Article? Article { get; set; }
+    }
+}
---
+++ GraduationProject_DAL/Repositories/ArticleRepository.cs
@@ -0,0 +1,54 @@
+﻿using GraduationProject_DAL.Data.Context;
+using GraduationProject_DAL.Data.Models;
+using GraduationProject_DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduationProject_DAL.Repositories
+{
+    public class ArticleRepository : IRepository<Article>
+    {
+        private readonly HospitalDBContext context;
+
+        public ArticleRepository(HospitalDBContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<List<Article>> GetAllAsync()
+        {
+            return await context.Articles.Include(a => a.Image).ToListAsync();
+        }
+
+        public async Task InsertAsync(Article item)
+        {
+            if(item!=null)
+            {
+                await context.AddAsync(item);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateAsync(int id, Article item)
+        {
+            var article = await context.Articles.FindAsync(id);
+            if (article != null)
+            {
+                article.Title = item.Title;
+                article.Description = item.Description;
+                article.Image = article.Image;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var article = await context.Articles.FindAsync(id);
+            if (article != null)
+            {
+                context.Remove(article);
+                await context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
