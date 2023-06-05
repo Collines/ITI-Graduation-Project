1cde7cc206477554f24f097585fee56d22aac819
Nada Ayman <76060283+nadaayman20@users.noreply.github.com>
2023-06-05 22:35:23 +0300

    Adding news-aside components

---
+++ Client/src/app/components/news-aside/news-aside.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-aside',
+  templateUrl: './news-aside.component.html',
+  styleUrls: ['./news-aside.component.css']
+})
+export class NewsAsideComponent {
+
+}
