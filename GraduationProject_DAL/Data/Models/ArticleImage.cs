860c4a065b12ba30bb73953772e3d0fd85647252
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-06-05 20:01:28 +0300

    Added Article Manager

--- GraduationProject_DAL/Data/Models/ArticleImage.cs
+++ GraduationProject_DAL/Data/Models/ArticleImage.cs
@@ -8,7 +8,7 @@ namespace GraduationProject_DAL.Data.Models
         public required string Name { get; set; }
 
         [ForeignKey("Article")]
-        public required int ArticleId { get; set; }
+        public int ArticleId { get; set; }
 
         public virtual Article? Article { get; set; }
 
