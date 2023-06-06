9294f00b2ba228ef34a15fb4335a2b94d7bbed86
Nada Ayman <76060283+nadaayman20@users.noreply.github.com>
2023-06-06 20:41:53 +0300

    Update news in home

--- Client/src/app/components/home-news/home-news.component.ts
+++ Client/src/app/components/home-news/home-news.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from 'src/app/Interfaces/Article';
 
 @Component({
   selector: 'app-home-news',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-news.component.css']
 })
 export class HomeNewsComponent {
-
+@Input() new:Article|null= null;
 }
