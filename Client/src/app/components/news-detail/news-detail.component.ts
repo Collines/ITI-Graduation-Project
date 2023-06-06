e0c9e0bd1bfbb7a84fa3ca0122d8e92eeb7414ff
Nada Ayman <76060283+nadaayman20@users.noreply.github.com>
2023-06-06 18:46:26 +0300

    Linking news details with database

--- Client/src/app/components/news-detail/news-detail.component.ts
+++ Client/src/app/components/news-detail/news-detail.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ArticleService } from './../../Services/article.service';
+import { Article } from 'src/app/Interfaces/Article';
+
 
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
   styleUrls: ['./news-detail.component.css']
 })
-export class NewsDetailComponent {
+export class NewsDetailComponent implements OnInit  {
+  ID: number;
+  article: Article = {
+    id: 0,
+    title: '',
+   description:'',
+    image: '',
+    postedAt: ''
+  };
 
-}
+  constructor(
+    private articleService: ArticleService,
+    private Route: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.ID = this.Route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.articleService.getById(this.ID).subscribe({
+      next: (res) => {
+        this.article = res;
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
+  }
+
